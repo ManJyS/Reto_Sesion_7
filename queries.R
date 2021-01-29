cb50eadab5cabc7e2d0e3da41fa4cc609db0a993
Manu <gori.uchiha@gmail.com>
2021-01-29 02:47:26 +0000

    #falta agregar complementos a las graficas

---
+++ queries.R
@@ -0,0 +1,18 @@
+# Crea un repositorio en Github llamado Reto_Sesion_7
+# 
+# Crea un Project llamado Reto_Sesion_07 dentro de RStudio utilizando tu cuenta 
+# de RStudio, que esté ligado al repositorio recién creado
+# 
+# Ahora en RStudio crea un script llamado queries.R en donde se conecte a 
+# la BDD shinydemo
+# 
+# Una vez hecha la conexión a la BDD, generar una busqueda con dplyr que 
+# devuelva el porcentaje de personas que hablan español en todos los países
+# 
+# Realizar una gráfica con ggplot que represente este porcentaje de tal 
+# modo que en el eje de las Y aparezca el país y en X el porcentaje, y 
+# que diferencíe entre aquellos que es su lengua oficial y los que no 
+# con diferente color (puedes utilizar la geom_bin2d() y coord_flip())
+# 
+# Una vez hecho esto hacer el commit y push para mandar tu archivo al 
+# repositorio de Github Reto_Sesion_7
